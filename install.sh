7bfa4c1882d20ca16cd105fd1b79cd73922a12b3
24SevenOffice Docker Robot <dockerrobot@24sevenoffice.com>
2016-02-17 12:26:26 +0100

    sdsdsd

--- install.sh
+++ install.sh
@@ -1,2 +1,9 @@
+apk add --update \
+    python \
+    python-dev \
+    py-pip \
+    build-base \
+  && pip install virtualenv \
+  && rm -rf /var/cache/apk/*
 echo "installing node-gyp"
 npm install node-gyp -g
